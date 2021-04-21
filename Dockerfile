68f7da367da5207b8521e03e35f5097d8417c017
myrlagksruf <myrlagksruf@gmail.com>
2021-04-21 18:19:29 +0900

    결국 회기

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,12 @@ FROM node:latest
 
 RUN apt-get update
 RUN apt-get -y upgrade
-# RUN apt-get install -y nodejs
 RUN apt-get install -y git
-RUN apt-get install -y certbot
 
 RUN npm i -g npm
 RUN npm i -g nodemon
 RUN npm i -g concurrently
+RUN npm i -g typescript
 
 WORKDIR /usr/src/app
 
@@ -17,12 +16,6 @@ RUN mkdir /usr/src/script
 ADD ./docker.sh /usr/src/script/docker.sh
 
 RUN chmod 777 /usr/src/script/docker.sh
-
-# COPY package*.json ./
-
-# RUN npm install
-
-# COPY . .
 
 ENTRYPOINT ["/usr/src/script/docker.sh"]
 
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import main from './main.js';
 import express from 'express';
 import test from './test.js';
 import fs from 'fs';
-import process from 'process';
-import https from 'https';
 const app = express();
 
 app.use(express.urlencoded({
@@ -18,14 +16,4 @@ app.use('/.well-known/acme-challenge/:id', async (req, res) => {
 app.use('/video/test', test);
 app.use('/video', main);
 
-if(process.argv[2] === 'https'){
-    console.log('https 가동');
-    const options = {
-        key: fs.readFileSync(`/etc/letsencrypt/live/${process.argv[3]}/privkey.pem`, {encoding:"utf-8"}),
-        cert: fs.readFileSync(`/etc/letsencrypt/live/${process.argv[3]}/cert.pem`, {encoding:"utf-8"}),
-        ca: fs.readFileSync(`/etc/letsencrypt/live/${process.argv[3]}/chain.pem`)
-    }
-    https.createServer(options, app).listen(5000);
-} else {
-    app.listen(5000);
-}
+app.listen(5000);
