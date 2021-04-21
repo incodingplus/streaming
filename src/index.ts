1a6232a8f669cec231e973689ebba1caab22487b
root <root@DESKTOP-OL6SOU2.localdomain>
2021-04-21 16:40:56 +0900

    let4

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import main from './main.js';
 import express from 'express';
 import test from './test.js';
+import fs from 'fs';
 const app = express();
 app.use(express.urlencoded({
     extended:true
 }));
 app.use(express.json());
-app.use('/.well-known/acme-challenge/:id', (req, res) => {
-    console.log(req.originalUrl);
-    res.end('good');
+app.use('/.well-known/acme-challenge/:id', async (req, res) => {
+    const txt = await fs.promises.readFile('./logs/let.txt', {encoding:'utf-8'});
+    res.end(`${req.params.id}.${txt}`);
 });
 
 app.use('/video/test', test);
