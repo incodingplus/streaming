5d23fcad31f465cdc3f42e6cd405b4c02907b11d
Seongjun Mun <colonist4@gmail.com>
2021-12-16 13:23:53 +0900

    fix: 인코딩 시간 구하는 로직 버그 개선

--- src/router/encode.ts
+++ src/router/encode.ts
@@ -65,7 +65,7 @@ const invokeEncodeLambdaFunction = async ({ bucket }: S3Context, key: string, st
 
 const convertSecondsTohhmmss = (seconds: number) => {
     const hh = Math.floor(seconds / 3600).toString().padStart(2, '0')
-    const mm = Math.floor(seconds / 60).toString().padStart(2, '0')
+    const mm = Math.floor((seconds % 3600) / 60).toString().padStart(2, '0')
     const ss = Math.floor(seconds % 60).toString().padStart(2, '0')
     return `${hh}:${mm}:${ss}`
 }
